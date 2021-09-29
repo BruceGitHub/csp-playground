57fdf0f29eb6da35bdb004491e37e9be99388262
BruceGitHub <roberto.diana@gmail.com>
2021-09-29 21:16:00 +0200

    Update test.php

--- test.php
+++ test.php
@@ -6,7 +6,7 @@ if (isset($_POST['header'])) {
 
     $vals = [];
     $vals[] = 'Content-Security-Policy-Report-Only: ';
-    $vals[] = "report-uri http://127.0.0.1:8080/report.php;";
+    $vals[] = "report-uri http://0.0.0.0:8080/report.php;";
     $csp = '';
 
     foreach ($vals as $v) {
@@ -32,13 +32,13 @@ if (isset($_POST['header'])) {
 <body>
     <form method="POST" id="form">
         <h3>Header (es: script-src 'none';) </h3>
-        <textarea rows="8" cols="90" name='header'><?php if (isset($_POST['header'])) echo ($_POST['header']); ?></textarea>
+        <textarea rows="10" cols="88" name='header'><?php if (isset($_POST['header'])) echo ($_POST['header']); ?></textarea>
 
         <h3>Tag Head (es: &lt;script nonce='AABBCCDD'&gt;foo(); &lt;/script&gt;)</h3>
-        <textarea rows="8" cols="90" name='head'><?php if (isset($_POST['head'])) echo ($_POST['head']); ?></textarea>
+        <textarea rows="10" cols="88" name='head'><?php if (isset($_POST['head'])) echo ($_POST['head']); ?></textarea>
 
         <h3>Tag Body (Some html tag)</h3>
-        <textarea rows="8" cols="90" name='body'><?php if (isset($_POST['body'])) echo ($_POST['body']); ?></textarea>
+        <textarea rows="10" cols="88" name='body'><?php if (isset($_POST['body'])) echo ($_POST['body']); ?></textarea>
         <br>
         <?php if (isset($_POST['body'])) echo ($_POST['body']); ?>
     </form>
