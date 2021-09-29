605daa1a920741bf57d5c0da2b757beae76d0259
BruceGitHub <roberto.diana@gmail.com>
2021-09-29 21:15:04 +0200

    Some fix

--- getViolations.php
+++ getViolations.php
@@ -18,35 +18,42 @@ $violationsSlot[0]->{"Violations number"} = \count($violationsSlot) - 1;
 <html>
 
 <head>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
 </head>
 
 <body>
-    <script>
-        function emptyHandler() {
-            fetch('http://0.0.0.0:8080/getViolations.php?p=del')
-            reloadHandler()
-        }
+<script>
+    function emptyHandler() {
+        fetch('http://0.0.0.0:8080/getViolations.php?p=del')
+        reloadHandler()
+    }
 
-        function reloadHandler() {
-            self.location.reload();
-        }
-    </script>
-    <hr class="my-2" />
-    <input class="btn btn-outline-primary" type='button' value="Reload" id="reload" onclick="javascript:reloadHandler()" />
-    <input class="btn btn-outline-primary" type='button' value="Empty log" id="empty" onclick="javascript:emptyHandler()" />
-    <hr class="my-2" />
-    <ul class="list-group fs-6">
-        <?php
-        foreach ($violationsSlot as $v) {
-            echo '<li class="list-group-item">';
-            foreach ($v as $k => $val) {
-                echo "| $k = $val <br>";
+    function reloadHandler() {
+        self.location.reload();
+    }
+</script>
+<div class="container m-0">
+    <div class="row">
+        <div class="col">
+            <hr class="my-2"/>
+            <input class="btn btn-outline-primary" type='button' value="Reload" id="reload"
+                   onclick="javascript:reloadHandler()"/>
+            <input class="btn btn-outline-primary" type='button' value="Empty log" id="empty"
+                   onclick="javascript:emptyHandler()"/>
+            <hr class="my-2"/>
+            <?php
+            foreach ($violationsSlot as $v) {
+                echo '<div class="card  text-white bg-danger mb-3""><div class="card-body">';
+                foreach ($v as $k => $val) {
+                    echo "<span style='font-weight: bold'>$k: </span> $val<br> ";
+                }
+                echo "</div></div><br>";
             }
-            echo "</li><br>";
-        }
-        ?>
-    </ul>
+            ?>
+        </div>
+    </div>
+</div>
 </body>
 
 </html>
